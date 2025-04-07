6086041f711d0e98f0638451115280f29390e90a
rajeev210403 <thotarajeev2003@gmail.com>
2025-04-07 18:25:06 +0530

    edited experience

--- components/experience/Experience.jsx
+++ components/experience/Experience.jsx
@@ -16,9 +16,55 @@ const Experience = () => {
         <span className="section__subtitle">What I work as</span>
 
         <div className="exp__container container grid">
+
+        <div className="exp__content">
+                <div>
+                    {/* <i className="uil uil-web-grid exp__icon"></i> */}
+                    <img className="exp__icon" src="../../src/assets/rashmiLogo.jpg" alt="" />
+                    <h3 className="exp__title">Product Coordinator <br /> Intern <br /> </h3>
+                </div>
+
+                <div>
+                    <span className="exp__comp">Rashmi Group</span>
+                </div>
+
+                <span className="exp__button" onClick={() => toggleTab(1)}>View More <i className="uil uil-arrow-right exp__button-icon"></i></span>
+
+                <div className={toggleState === 1 ? "exp__modal active-modal" : "exp__modal"}>
+                    <div className="exp__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times exp__modal-close"></i>
+
+                        <h3 className="exp__modal-title">Product coordinator Intern <br /> Jan'25 - Present </h3>
+                        <p className="exp__modal-description">Working as a Product Coordinator at Kalgudi Digital.
+                            Providing quality work to company.
+                        </p>
+
+                        <ul className="exp__modal-experiences grid">
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info"> Built a real-time ERP analytics dashboard webpage by fetching live data from Google Sheets into a centralized manage
+                                ment view.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Coordinating the Workday HCM implementation, ensuring smooth transition and integration.</p>
+                            </li>
+
+                            <li className="exp__modal-experience">
+                                <li className="uil uil-check-circle exp__modal-icon"></li>
+                                <p className="exp__modal-info">Gathered requirements & designed process flows for HRMS module by collaborating with the HR team.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
             <div className="exp__content">
                 <div>
-                    <i className="uil uil-web-grid exp__icon"></i>
+                    {/* <i className="uil uil-web-grid exp__icon"></i> */}
+                    <img className="exp__icon" src="../../src/assets/kalgudiLogo.jpeg" alt="" />
                     <h3 className="exp__title">SDE <br /> Intern <br /> </h3>
                 </div>
 
@@ -61,7 +107,8 @@ const Experience = () => {
 
             <div className="exp__content">
                 <div>
-                    <i className="uil uil-arrow exp__icon"></i>
+                    {/* <i className="uil uil-arrow exp__icon"></i> */}
+                    <img className="exp__icon" src="../../src/assets/rishaLogo.jpeg" alt="" />
                     <h3 className="exp__title">Research <br /> Intern <br/> </h3>
                 </div>
 
